8059a2476634c806d52d215fccf0bcc9578b0875
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-04 23:05:19 +0700

    update files

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const express = require('express')
-const axios = require('axios');
-const cors = require('cors');
-const logger = require('morgan');
-const app = express();
+const axios = require('axios')
+const cors = require('cors')
+const logger = require('morgan')
+const app = express()
 
 app.use(express.json())
-app.use(cors());
-app.use(logger('dev'));
-app.enable('trust proxy');
-app.disable('x-powered-by');;
+app.use(cors())
+app.use(logger('dev'))
+app.enable('trust proxy')
+app.disable('x-powered-by')
 
 app.get('/api/v2/users', async (req, res) => {
-	const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
-	res.json(data);
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+  res.json(data)
 })
 
-app.listen(3001, () => console.log('server is running on port 3001'));
+module.exports = { app }
---
+++ server.js
@@ -0,0 +1,14 @@
+const { app } = require('./app')
+const server = require('express')()
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+server.use(
+  createProxyMiddleware({
+    target: 'http://localhost:1945',
+    changeOrigin: true,
+    xfwd: true
+  })
+)
+
+server.listen(process.env.PROXY_PORT, () => console.log('proxy server is running on port 3000'))
+app.listen(process.env.ORIGINAL_PORT, () => console.log('original server is running on port 1945'))
